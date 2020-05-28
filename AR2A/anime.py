abd195a67c731ccc3cf83abeb018c878d2f4a030
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-05-28 17:47:00 +0800

    Update

--- AR2A/anime.py
+++ AR2A/anime.py
@@ -6,6 +6,7 @@ import time
 
 import feedparser
 import yaml
+import requests
 from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -29,9 +30,15 @@ class Downloaded(Base):
 
 class Anime():
 
-    def __init__(self, rssFile, dataFile):
+    def __init__(self, rssFile, dataFile, aria2 = None):
         self.rssFile = rssFile
         self.dataFile = dataFile
+        self.aria2 = aria2
+
+        if self.aria2 == None:
+            self.send = False
+        else:
+            self.send = True
 
         if os.path.exists(self.rssFile):
             with open(self.rssFile, 'r', encoding='UTF-8') as f:
@@ -49,12 +56,11 @@ class Anime():
         Base.metadata.create_all(engine)
         self.DBSession = sessionmaker(bind=engine)
 
-        self.urls = {}
+    def readRSS(self, send = 'default'):
+        if not send == 'default':
+            self.send = send
 
-    def readRSS(self):
         for s in self.rss:
-            self.urls[s['path']] = []
-
             if s['source'].lower() == 'mikan':
                 self.readMikan(s)
             elif s['source'].lower() == 'rarbg':
@@ -69,9 +75,9 @@ class Anime():
             if regex.match(r['title']) and not session.query(exists().where(Downloaded.title == r['title'])).scalar():
                 for l in r['links']:
                     if l['type'] == 'application/x-bittorrent':
-                        self.urls[a['path']].append(l['href'])
-                        self.addDownload(session, a['series'], r['title'], l['href'])
-                        session.close()
+                        if self.send2Aria2(a['path'], l['href']):
+                            self.addDownload(session, a['series'], r['title'], l['href'])
+                            session.close()
 
     def readRarbg(self, a):
         entries = feedparser.parse(a['url'])['entries']
@@ -80,12 +86,46 @@ class Anime():
 
         for r in entries:
             if regex.match(r['title']) and not session.query(exists().where(Downloaded.title == r['title'])).scalar():
-                self.urls[a['path']].append(r['link'])
-                self.addDownload(session, a['series'], r['title'], r['link'])
-                session.close()
+                if self.send2Aria2(a['path'], r['link']):
+                    self.addDownload(session, a['series'], r['title'], r['link'])
+                    session.close()
 
     def addDownload(self, session, anime, title, link):
         ticks = time.time()
         new_ep = Downloaded(anime=anime, title=title, link=link, create_time=str(ticks))
         session.add(new_ep)
         session.commit()
+
+    def send2Aria2(self, path, url):
+        if not self.send:
+            print('未添加Aria2客户端或仅作为测试！链接为: ', url)
+            return False
+
+        else:
+            if 'magnet:?xt=' in url:
+                try:
+                    rs = self.aria2.add_magnet(url, options={'dir': path})
+                except:
+                    print('添加失败 Magnet: ', url)
+                    print(rs)
+                    return False
+                else:
+                    print('添加成功 Magnet: ', url)
+                    return True
+
+            else:
+                r = requests.get(url)
+                with open('tmp.torrent', 'wb') as f:
+                    f.write(r.content)
+
+                try:
+                    rs = self.aria2.add_torrent('tmp.torrent', options={'dir': path})
+                except:
+                    print('添加失败 Torrent: ', url)
+                    print(rs)
+                    os.remove('tmp.torrent')
+                    return False
+                else:
+                    print('添加成功 Torrent: ', url)
+                    os.remove('tmp.torrent')
+                    return True
